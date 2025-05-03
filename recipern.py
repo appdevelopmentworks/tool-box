e2a3760868f95545e8a226e0f2ac97ddca471a29
appdevelopmentworks <81832175+appdevelopmentworks@users.noreply.github.com>
2025-05-03 17:52:36 +0900

    recipern 1st

--- recipern.py
+++ recipern.py
@@ -7,11 +7,35 @@ from PIL import Image
 import io
 
 
+# API キーをグローバルに設定
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+#モデルを指定
+model = genai.GenerativeModel(model_name="gemini-2.5-pro-exp-03-25")
 
 
 
+def generate_recipe(image_part):
+    try:
+        
+        contents = [
+            {
+                "role": "user",
+                "parts": [
+                    image_part,
+                    {"text": "写真イメージに写っている食材を使ってレシピを考えてください。"},
+                ],
+            }
+        ]
 
-
+        response = model.generate_content(contents, stream=True)
+        
+        # for chunk in response:
+        #     print(chunk.text, end="", flush=True)
+        #st.write()  # 最後の改行
+        return response
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+        return None
 
 
 
@@ -31,12 +55,26 @@ st.caption("カメラで食材を撮るか画像ファイルをアップロー�
 
 upfile = st.file_uploader("食材の写った画像をアップロード", type=["jpg", "jpeg", "png"])
 
-image = st.camera_input("カメラで食材を撮影してください")
+imagecam = st.camera_input("カメラで食材を撮影してください")
+
+
 
 if upfile is not None:
     image = Image.open(upfile)
     st.image(image, caption="アップロードされた画像", use_column_width=True)
+    #st.code(generate_recipe(image), language='markdown')
+    res = generate_recipe(image)
+    for chunk in res:
+        st.write(chunk.text, end="", flush=True)
+    st.write()  # 最後の改行
     
-if image is not None:
+if imagecam is not None:
     # カメラで撮影した場合の処理
-    st.image(image, caption='撮影した画像', use_container_width=True)
+    #st.image(image, caption='撮影した画像', use_container_width=True)
+    st.image(imagecam, caption='撮影した画像', use_column_width=True)
+    #st.code(generate_recipe(image), language='markdown')
+    #generate_recipe(image)
+    res = generate_recipe(imagecam)
+    for chunk in res:
+        st.write(chunk.text, end="", flush=True)
+    st.write()  # 最後の改行
